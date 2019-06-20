d9e09af727f346c2cab2d542861a06937b5b08eb
suat.ogme <suat.ogme@obss.com.tr>
2019-06-20 19:38:42 +0300

    .babelrc replaced with babel.config.js, webpack configuration improvements for new babel.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,64 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-const htmlPlugin = new HtmlWebPackPlugin({
-  template: "./src/index.html",
-  filename: "./index.html"
-});
-
 const extractPlugin = new ExtractTextPlugin({
-  filename: 'main.css'
+    filename: 'main.css'
 });
 
 module.exports = {
     module: {
-      rules: [{
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
-        }, {
-          test: /\.scss$/,
-          use: extractPlugin.extract({
-            use: ['css-loader', 'sass-loader']
-          })
-        }
-      ]
+        rules: [
+            {
+                test: /\.html$/,
+                use: [{
+                    loader: "html-loader",
+                    options: {minimize: true}
+                }]
+            }, {
+                test: /\.js$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', {
+                                targets: {
+                                    browsers: ["last 2 Chrome versions"]
+                                }
+                            }]
+                        ]
+                    }
+                }
+            }, {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [{
+                    loader: "file-loader",
+                    options: {
+                        outputPath: "images",
+                        name: "[name].[ext]"
+                    }
+                }]
+            }, {
+                test: /\.(eot|ttf|woff|woff2)$/,
+                use: [{
+                    loader: "file-loader",
+                    options: {
+                        outputPath: "fonts",
+                        name: "[name].[ext]"
+                    }
+                }]
+            }, {
+                test: /\.scss$/,
+                use: extractPlugin.extract({
+                    use: ['css-loader', 'sass-loader']
+                })
+            }
+        ]
     },
-    plugins: [htmlPlugin, extractPlugin]
-  };
+
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }), extractPlugin
+    ]
+};
