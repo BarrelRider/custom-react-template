a7d8e1403cea8ec440f2aa044fc6e774cba1b6df
suat.ogme <suat.ogme@obss.com.tr>
2019-06-20 19:40:33 +0300

    importing images before the components for relative img[src=] usage

--- src/index.js
+++ src/index.js
@@ -4,4 +4,9 @@ import App from './components/App'
 
 import "./assets/scss/main.scss"
 
+function importAll(r) {
+    return r.keys().map(r);
+}
+importAll(require.context('./', true, /\.(png|jpe?g|svg|eot|ttf|woff|woff2)$/));
+
 ReactDOM.render(<App />, document.getElementById("index"));
